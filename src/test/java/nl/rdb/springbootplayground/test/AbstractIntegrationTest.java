cfbd096e84cdda7f40da66d589355a9f346a8747
Robin de Boer <robin.de.boer@42.nl>
2026-01-26 15:48:03 +0100

    Updated the logback-spring.xml
    - Updated to spring boot 4.0.2
    - Added initSystemAuth for the use in tests.

--- src/test/java/nl/rdb/springbootplayground/test/AbstractIntegrationTest.java
+++ src/test/java/nl/rdb/springbootplayground/test/AbstractIntegrationTest.java
@@ -1,6 +1,10 @@
 package nl.rdb.springbootplayground.test;
 
+import static org.springframework.security.core.context.SecurityContextHolder.getContext;
+
+import nl.rdb.springbootplayground.config.security.AuthenticationAdapter;
 import nl.rdb.springbootplayground.shared.docker.mail.MailpitClient;
+import nl.rdb.springbootplayground.user.User;
 import tools.jackson.databind.json.JsonMapper;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -24,5 +28,9 @@ public abstract class AbstractIntegrationTest {
     @BeforeEach
     void clearMailHogInbox() {
         mailpitClient.deleteAll();
+    }
+
+    protected void initSystemAuth() {
+        getContext().setAuthentication(new AuthenticationAdapter(User.SYSTEM_USER));
     }
 }
